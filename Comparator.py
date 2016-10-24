7dc059e4479a798ad32ff4e58a398e7e898efc8e
Haoming Liu <haoming9502@gmail.com>
2016-10-24 16:45:26 -0700

    add more samples and some changes in comparator

--- Comparator.py
+++ Comparator.py
@@ -13,26 +13,45 @@ import matplotlib.pyplot as plt
 #                to a ReplayGain value
 #  - EqloudLoader: an EasyLoader that applies an equal-loudness filtering on the audio
 #
-loader = essentia.standard.MonoLoader(filename='sample/two_tigers_sample.mp3')
-audio = loader() # load the music
+# loader = essentia.standard.MonoLoader(filename='sample/two_tigers_sample.mp3')
+loader_filtered = essentia.standard.MonoLoader(filename='sample/hill_sample_filtered.mp3')
+# audio = loader() # load the music
+audio_filtered = loader_filtered()
 
 window = essentia.standard.Windowing(type = 'hann') # window function
 spectrum = essentia.standard.Spectrum()  # spectrum function
 pitch_yin_fft = essentia.standard.PitchYinFFT() # pitch extractor
 
 index = 0
-pitches = []
+# pitches = []
+pitches_filtered = []
+frames = []
 
 # Extract pitches for each frame
-for frame in essentia.standard.FrameGenerator(audio, frameSize = 4096, hopSize = 512):
-	sample_spectrum = spectrum(window(frame))
+for frame_filtered in essentia.standard.FrameGenerator(audio_filtered, frameSize = 2048, hopSize = 512):
+	# sample_spectrum = spectrum(frame)
+	# pitch, pitch_confidence = pitch_yin_fft(sample_spectrum)
+	# pitches.append(pitch)
+	for f in frame_filtered:
+		frames.append(f)
+
+	sample_spectrum = spectrum(window(frame_filtered))
 	pitch, pitch_confidence = pitch_yin_fft(sample_spectrum)
-	pitches.append(pitch)
+
+	if pitch_confidence < 0.8:
+		print "{}: {}".format(pitch, pitch_confidence)
+		pitches_filtered.append(0.0)
+	else:
+		pitches_filtered.append(pitch)
+
 	index = index + 1
 
-
-plt.plot(pitches)
+plt.plot(frames)
 plt.show()
+
+# plt.plot(range(len(pitches_filtered)), pitches_filtered, label='filtered')
+# plt.plot(range(len(pitches)), pitches, label='original')
+# plt.show()
 
 
 
