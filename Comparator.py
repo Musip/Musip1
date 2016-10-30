5b7792da73f15040b32125c631a9188b5e1ab168
Haoming Liu <haoming9502@gmail.com>
2016-10-29 22:08:52 -0700

    add dp solution

--- Comparator.py
+++ Comparator.py
@@ -25,3 +25,37 @@ def draw_plot(frames, window, spectrum, pitch_yin_fft, pitch_saliennce):
 
 	plt.plot(range(len(pitches)), pitches, label='pitch')
 	plt.show()
+
+def sequence_alignment(array1, array2, padding):
+	height = len(array1)
+	width = len(array2)
+	result_matrix = [[0 for x in range(width)] for y in range(height)]
+
+	for i in range(height):
+		result_matrix[0][i] = i;
+
+	for j in range(width):
+		result_matrix[j][0] = j;
+
+	for i in range(height):
+		for j in range(width):
+			match = 0 if abs(array1[i] - array2[j]) < padding else 1
+			result_matrix[i][j] = min(match + result_matrix[i-1][j-1], \
+									  1 + result_matrix[i-1][j], \
+									  1 + result_matrix[i][j-1])
+
+	return result_matrix[height-1][width-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
