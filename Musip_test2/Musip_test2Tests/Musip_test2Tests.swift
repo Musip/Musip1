3eaba1a960917300c98eee525c3b44afeb0dc9d8
Haoming Liu <haoming9502@gmail.com>
2016-11-04 13:42:17 -0700

    recommit

---
+++ Musip_test2/Musip_test2Tests/Musip_test2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Musip_test2Tests.swift
+//  Musip_test2Tests
+//
+//  Created by Haoming Liu on 11/4/16.
+//  Copyright © 2016 Haoming Liu. All rights reserved.
+//
+
+import XCTest
+@testable import Musip_test2
+
+class Musip_test2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
