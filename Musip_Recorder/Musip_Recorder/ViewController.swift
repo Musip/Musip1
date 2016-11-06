a2b123502c68ed31fbf6ee27cbcb715c1a8f59fc
Haoming Liu <haoming9502@gmail.com>
2016-11-05 23:41:15 -0700

    add uploader

--- Musip_Recorder/Musip_Recorder/ViewController.swift
+++ Musip_Recorder/Musip_Recorder/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
     
     @IBOutlet var RecordButton: UIButton!
     @IBOutlet var PlayButton: UIButton!
+    @IBOutlet weak var UploadButton: UIButton!
     
     var recorder : AVAudioRecorder!
     var player : AVAudioPlayer!
@@ -54,7 +55,6 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
     
     func getFileUrl() -> URL {
         let path = getCacheDirectory().appendingPathComponent(fileName)
-        
         return path
     }
     
@@ -98,6 +98,99 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
     func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
         PlayButton.setTitle("Play", for: .normal)
         RecordButton.isEnabled = true
+    }
+    
+    @IBAction func uploadAudio(_ sender: UIButton) {
+        let destURL = NSURL(string: "http://www.iamhaomingliu.com"); // change later
+        let filePathURL = NSURL(fileURLWithPath: fileName)
+        
+        let request = NSMutableURLRequest(url:destURL! as URL);
+        request.httpMethod = "POST";
+        
+        let param = [
+            "firstName"  : "Haoming",
+            "lastName"    : "Liu",
+            "userId"    : "0"
+        ]
+        
+        let boundary = generateBoundaryString()
+        
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        
+        
+        let audioData = NSData(contentsOf: filePathURL as URL)
+        
+        if(audioData == nil) {
+            return;
+        }
+        
+        request.httpBody = createBodyWithParameters(parameters: param, filePathKey: "file", audioDataKey: audioData!, boundary: boundary) as Data
+        
+        
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+            if error != nil {
+                print("error=\(error)")
+                return
+            }
+            
+            // Print out response object
+            print("******* response = \(response)")
+            
+            // Print out reponse body
+            let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            print("****** response data = \(responseString!)")
+            
+            do {
+                _ = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary
+            } catch {
+                print(error)
+            }
+            
+        }
+        
+        task.resume()
+    }
+    
+    func generateBoundaryString() -> String {
+        return "Boundary-\(NSUUID().uuidString)"
+    }
+    
+    func createBodyWithParameters(parameters: [String: String]?, filePathKey: String?, audioDataKey: NSData, boundary: String) -> NSData {
+        let body = NSMutableData();
+        
+        if parameters != nil {
+            for (key, value) in parameters! {
+                body.appendString(string: "--\(boundary)\r\n")
+                body.appendString(string: "Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")
+                body.appendString(string: "\(value)\r\n")
+            }
+        }
+        
+        let filename = fileName
+        let mimetype = "audio/mpeg"
+        
+        body.appendString(string: "--\(boundary)\r\n")
+        body.appendString(string: "Content-Disposition: form-data; name=\"\(filePathKey!)\"; filename=\"\(filename)\"\r\n")
+        body.appendString(string: "Content-Type: \(mimetype)\r\n\r\n")
+        body.append(audioDataKey as Data)
+        body.appendString(string: "\r\n")
+        
+        
+        
+        body.appendString(string: "--\(boundary)--\r\n")
+        
+        return body
+    }
+}
+
+extension NSMutableData {
+    
+    func appendString(string: String) {
+        let data = string.data(using: String.Encoding.utf8, allowLossyConversion: true)
+        append(data!)
     }
 }
 
